817f361b46f419e0edc0595fc0bb1d7713399822
IanTayler <iangtayler@gmail.com>
2022-02-24 14:22:07 -0300

    program_rom: improve program style

--- culebra.srcs/sources_1/new/program_rom.sv
+++ culebra.srcs/sources_1/new/program_rom.sv
@@ -39,15 +39,15 @@ module program_rom
         case (address_reg)
             // Should get 4 LEDs with different brightness.
             //              LOAD    A         [PAD]            0
-            'h00: memory = {5'h5, 3'h0, (WIDTH-8)'('b0), WIDTH'('h0)};
+            'h00: memory = {5'h05, 3'h0, (WIDTH-8)'('b0), WIDTH'('h0)};
             //               XOR    A         [PAD]            15
-            'h01: memory = {5'h4, 3'h0, (WIDTH-8)'('b0), WIDTH'('hf)};
+            'h01: memory = {5'h04, 3'h0, (WIDTH-8)'('b0), WIDTH'('hf)};
             //               AND    A         [PAD]            10
-            'h02: memory = {5'h2, 3'h0, (WIDTH-8)'('b0), WIDTH'('ha)};
+            'h02: memory = {5'h02, 3'h0, (WIDTH-8)'('b0), WIDTH'('ha)};
             //               AND    A         [PAD]            8
-            'h03: memory = {5'h2, 3'h0, (WIDTH-8)'('b0), WIDTH'('h8)};
+            'h03: memory = {5'h02, 3'h0, (WIDTH-8)'('b0), WIDTH'('h8)};
             //              LOAD    P         [PAD]            0
-            'h04: memory = {5'h5, 3'h5, (WIDTH-8)'('b0), WIDTH'('h0)}; // loop forever
+            'h04: memory = {5'h05, 3'h5, (WIDTH-8)'('b0), WIDTH'('h0)}; // loop forever
             default: memory = 'h00;
         endcase
     end
