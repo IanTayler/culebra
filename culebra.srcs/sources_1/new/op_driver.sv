1df61311a63d2a40f7005fda583255878ab07870
IanTayler <iangtayler@gmail.com>
2022-02-24 01:35:48 -0300

    op_driver: rework into 4-step pipeline.

--- culebra.srcs/sources_1/new/op_driver.sv
+++ culebra.srcs/sources_1/new/op_driver.sv
@@ -33,16 +33,52 @@ module op_driver
         input wire                 clk
     );
 
-    initial
+    // The pipeline is:
+    // 1. Read instruction_pointer, this sets the address.
+    // 2. Next clock cycle we're loading the instruction into memory.
+    // 3. Set op_enable.
+    // 4. Wait for the CPU to run. Then repeat.
+    reg set, loaded, ready;
+    reg [WIDTH*2-1 : 0] instruction;
+
+    initial begin
+        // This should actually be implied, but let's make it explicit.
         address = 'h00;
-    always @(posedge clk) begin
-        // Enable every cycle for now.
-        // NOTE: We may want to break some instructions into multiple cycles
-        // in the future, but not yet. Keep it simple for now.
-        op_enable = 'b1;
-        imm_reg = memory[WIDTH-1 : 0];
-        active_op = memory[WIDTH*2-1 : WIDTH*2-4];
-        active_reg = memory[WIDTH*2-5 : WIDTH*2-7];
-        address = instruction_pointer;
+        set = 'b0;
+        loaded = 'b0;
+        ready = 'b0;
+        op_enable = 'b0;
     end
+    always @(posedge clk) begin
+        // NOTE: We may want to break some instructions into multiple cycles
+        // in the future, but not yet.
+        if (ready) begin // Ready! Set new address.
+            if (set) begin // Address set! Wait until it loads.
+                if (loaded) begin // Loaded! Set instruction.
+                    // Load the instruction.
+                    instruction = memory;
+                    // Parse instruction.
+                    imm_reg = instruction[WIDTH-1 : 0];
+                    active_op = instruction[WIDTH*2-1 : WIDTH*2-4];
+                    active_reg = instruction[WIDTH*2-5 : WIDTH*2-7];
+                    // Set enable.
+                    op_enable = 'b1;
+                    // Reset pipeline state.
+                    loaded = 'b0;
+                    set = 'b0;
+                    ready = 'b0;
+                end else begin // Not loaded
+                    loaded = 'b1;
+                end
+            end else begin // Not set
+                // Load the address that will be used next cycle.
+                address = instruction_pointer;
+                set = 'b1;
+            end
+        end else begin // Not ready
+            ready = 'b1;
+            op_enable = 'b0;
+        end
+    end
+
 endmodule
